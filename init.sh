d90fce7ae738421a3840b3d5bb1def99bccac344
dugembot <72567572+dugembot@users.noreply.github.com>
2020-10-11 21:41:13 +0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,10 +6,10 @@ if [[ -n $SPRING_DATASOURCE_TYPE && -n $SPRING_DATASOURCE_URL ]]; then
 	echo "on-download-complete=./on-complete.sh" >> aria2.conf
 	chmod +x delete.sh
 	chmod +x on-complete.sh
-	touch aria2.session
+	touch aria2c.session
 fi
 
-echo "rpc-secret=$SPRING_DATASOURCE_PASSWORD" >> aria2c.conf
+echo "rpc-secret=$SPRING_DATASOURCE_PASSWORD" >> aria2.conf
 
 aria2c --conf-path=aria2.conf
 node index.js
